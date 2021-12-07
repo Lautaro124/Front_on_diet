99eb2a4256551ce82b36ead0a448f265dbed23cd
Lautaro Gonzalez <59624470+Lautaro124@users.noreply.github.com>
2021-12-07 18:40:56 -0300

    Home and back deploy

---
+++ src/components/home/images.js
@@ -0,0 +1,46 @@
+export const setImages = [
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638899201/On%20diet%20images%20/image1_zuj4oq.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638899792/On%20diet%20images%20/image2_mivzzn.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638900210/On%20diet%20images%20/image3_iehc5x.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638900359/On%20diet%20images%20/image4_smvdp4.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638900635/On%20diet%20images%20/image5_ouihsh.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638900740/On%20diet%20images%20/image6_gytloc.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638900946/On%20diet%20images%20/image7_bsjthk.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638901069/On%20diet%20images%20/image8_vhbiae.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638901156/On%20diet%20images%20/image9_nqdx8u.jpg',
+    
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638901238/On%20diet%20images%20/image10_wptuhr.jpg',
+  
+  },
+  {
+    url: 'https://res.cloudinary.com/dwvnbejfd/image/upload/v1638901319/On%20diet%20images%20/image11_izzncy.jpg',
+    
+}
+]
---
+++ src/utils/texts.js
@@ -0,0 +1,11 @@
+const empresa = 'On Diet'
+module.exports = {
+	textHome: {
+		title: `Bienvenidos a ${empresa}`,
+		firstText: `En ${empresa} nos preocupamos por la salud y el bienestar de todos nuestros clientes y elaboramos todas nuestras viandas de comidas light con alimentos frescos y de la mejor calidad, con cocineras expertas y con las más elevadas normas de higiene y seguridad.`,
+		secondText: `Con la tranquilidad y confianza de más de 20 años de experiencia nos especializamos en la elaboración y delivery de viandas a domicilio con recetas y dietas pensadas para una amplia gama de consumidores. Dentro de nuestras dietas para bajar de peso, contamos con dietas clásicas como las dietas scarsdale, dietas para diabéticos, dietas para embarazadas, dietas vegetarianas, dietas para freezer, dietas bajas en colesterol y dietas especiales para niños, deportistas y personas de tercera edad.`,
+		trheeText: `Al contratar el servicio de dietas a domicilio de ${empresa}, ud. se asegura de recibir en la comodidad de su casa o en su oficina, viandas light elaboradas y personalizadas según sus necesidades. Nuestro servicio incluye el delivery de dietas donde ud. lo disponga.`,
+		fourtyText: `Otra característica que destaca nuestro servicio de delivery de viandas, es que en nuestro staff contamos con la participación de una nutricionista con más de 15 años de trayectoria y profesionalismo que con una consulta previa podrá elaborar una dieta personalizada que se ajuste a sus necesidades y exigencias.`,
+		about: 'Saber mas de nosotros'
+	}
+}
